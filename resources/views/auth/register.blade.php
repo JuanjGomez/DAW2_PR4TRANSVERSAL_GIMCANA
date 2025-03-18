7f13445d3ccbc83f072a0dd21b0a8fa1887aa298
Aina Orozco Gonzalez <468417.joan23@fje.edu>
2025-03-18 20:43:15 +0100

    register validaciones js

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <title>Register</title>
+    <script src="{{ asset('js/register.js') }}" defer></script>
 </head>
 <body>
     <div class="container">
