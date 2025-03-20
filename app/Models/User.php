cb2c19715cfdf8d78b5ffc536077e1396e85e78f
juan <451586.joan23@fje.edu>
2025-03-20 16:55:28 +0100

    ajustes

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
      *
      * @var string
      */
-    protected $table = 'users';
+    protected $table = 'user';
 
     /**
      * The attributes that are mass assignable.
